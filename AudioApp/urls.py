3614ddf3946e170c89ff380c47d23a50a2b77490
Alex <zarjrw1994@gmail.com>
2022-09-02 23:46:33 +0300

    Adding routes: Password change, password change done, mail change, mail change done, account deletion.

--- AudioApp/urls.py
+++ AudioApp/urls.py
@@ -5,15 +5,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'), # Домашняя страница
+    path('about/', views.about, name='about'), # О нас
     path('user/', views.user_account, name='user_account'), # Кабинет пользователя
     path('login/',views.Login.as_view(), name='login'), # Авторизация
-    path('registration', views.RegisterUser.as_view(), name='user_reg'), # Регистрация
-    path('about/', views.about, name='about'), # О нас
-    path('settings/', views.user_settings, name='settings'), # Страница удаления аккаунта
+    path('registration/', views.RegisterUser.as_view(), name='user_reg'), # Регистрация
     path('userpage/<slug:slug>/', views.UserPage.as_view(),name='user_page'), # Страница пользователя
     path('logout/', views.logout_user, name='logout_user'), # Выход из аккаунта
-    path('password-change/', views.PasswordChange.as_view(), name='password_change'),
-    path('password-change/done/', views.PasswordChangeOk.as_view(), name='password_change_ok'),
+    path('settings/', views.user_settings, name='settings'), # Страница настроек
+    path('password-change/', views.PasswordChange.as_view(), name='password_change'), # Страница изменения пароля
+    path('password-change/done/', views.PasswordChangeOk.as_view(), name='password_change_ok'),  # Страница успешного изм пароля
+    path('email-change/', views.EmailChange.as_view(), name='email_change'), #  Страница изменения email
+    path('email-change/done/', views.email_change_done, name='email_change_ok'),
+    path('delete-page/', views.del_page, name='del_page')
 
 ]
 
