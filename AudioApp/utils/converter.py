85880ca1792b6ed09a9a131b00b5948d0f177c0d
Alex <zarjrw1994@gmail.com>
2022-10-07 16:40:47 +0300

    Adjusting the output of audio files depending on DEBUG.

--- AudioApp/utils/converter.py
+++ AudioApp/utils/converter.py
@@ -1,4 +1,4 @@
-from AudioConverter.settings import STATICFILES_DIRS
+from AudioConverter.settings import STATICFILES_DIRS, MEDIA_ROOT
 from ..audiohandler.audio import AudioConverter
 
 # Создаем объект конвертора, который будет отвечать за конвертацию аудиофайлов. Передаем настройки
@@ -7,11 +7,10 @@ settings = {
         'move': True, # Перемещать (не копировать) файлы в папку конвертированных
         'write_db': False, # Использовать стандартную базу данных конвертора
         'db_path': '', # Путь к стандартной базе данных конвертора
-        'storage_path': f'{STATICFILES_DIRS[0]}', # Путь, где будут храниться конвертированные файлы
+        'storage_path': f'{MEDIA_ROOT}', # Путь, где будут храниться конвертированные файлы
         }
 
 #object converter
-# print(STATICFILES_DIRS[0])
 converter = AudioConverter(setting_dict=settings)
 
 
--- AudioConverter/urls.py
+++ AudioConverter/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django.views.static import serve as mediaserve
+# from django.conf.urls import url
+from django.urls import re_path as url
+
 from AudioConverter import settings
+
 
 
 urlpatterns = [
@@ -9,17 +15,26 @@ urlpatterns = [
     path('captcha', include('captcha.urls')),
     path('', include('AudioApp.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)  # Загрузка файлов на локальном сервере
 
 handler404 = "AudioApp.views.error404"
 
-# Загрузка медиа файлов на локальном сервере .
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include('debug_toolbar.urls')),
     ] + urlpatterns
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)  # Загрузка файлов на локальном сервере
+
+else:
+    urlpatterns += [
+        url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.MEDIA_ROOT}),
+        url(f'^{settings.STATIC_URL.lstrip("/")}(?P<path>.*)$',
+            mediaserve, {'document_root': settings.STATIC_ROOT})
+    ]
 
 
 
