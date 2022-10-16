55d158f5b3c7772f93c3b5b59cb4e12d21f71a44
Alex <zarjrw1994@gmail.com>
2022-10-16 23:41:22 +0300

    Change the names of variables.

--- AudioApp/utils/database.py
+++ AudioApp/utils/database.py
@@ -6,24 +6,25 @@ def write_database(data_dict : dict) ->bool :
     """Сохраняет данные о треке в базу данных"""
 
     login :str = data_dict.get('user_name', '')
-    trek_name :str = data_dict.get('trek_name', '')
+    audio_name :str = data_dict.get('audio_name', '')
     original_format  :str = data_dict.get('original_format', '')
-    original_track :str = os.path.relpath(data_dict.get('path_original', ''))
-    convertable_track :str = os.path.relpath(data_dict.get('path_convert', ''))
-    convertable_format :str= data_dict.get('format', '')
-    original_track_size_b :int = data_dict.get('original_size_b', 0)
-    original_track_size_mb :int = data_dict.get('original_size_mb', 0)
-    convert_track_size_b :int = data_dict.get('convert_size_b', 0)
-    convert_track_size_mb :int = data_dict.get('convert_size_mb', 0)
+    original_path :str = data_dict.get('path_original', '')
+    convert_path :str = data_dict.get('path_convert', '')
+    convert_format :str= data_dict.get('format', '')
+    original_audio_size_b :int = data_dict.get('original_size_b', 0)
+    original_audio_size_mb :int = data_dict.get('original_size_mb', 0)
+    convert_audio_size_b :int = data_dict.get('convert_size_b', 0)
+    convert_audio_size_mb :int = data_dict.get('convert_size_mb', 0)
     # Дата и время конвретации формируется в модели автоматически.
     # date = data_dict.get('date', '')
 
     try:
-        database :AudioData = AudioData(login=login, trek_name=trek_name,
-                                        original_format=original_format,original_track=original_track,
-                                        convertable_track=convertable_track, convertable_format=convertable_format,
-                                        original_track_size_b=original_track_size_b, original_track_size_mb=original_track_size_mb,
-                                        convert_track_size_b=convert_track_size_b, convert_track_size_mb=convert_track_size_mb)
+        database :AudioData = AudioData(login=login, audio_name=audio_name, original_format=original_format,
+                                        original_path=original_path, convert_path=convert_path,
+                                        convert_format=convert_format, original_audio_size_b=original_audio_size_b,
+                                        original_audio_size_mb=original_audio_size_mb,
+                                        convert_audio_size_b=convert_audio_size_b,
+                                        convert_audio_size_mb=convert_audio_size_mb)
         #date=date
         database.save()
     except:
