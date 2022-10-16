4c2d25c7b00371a09d91336b4ead912f357146bb
Alex <zarjrw1994@gmail.com>
2022-10-16 23:59:17 +0300

    AudioData model. Changed field names in the database. Added new methods to the model.

--- AudioApp/models.py
+++ AudioApp/models.py
@@ -2,6 +2,7 @@ import os
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.http import FileResponse
 from django.urls import reverse
 
 
@@ -18,16 +19,16 @@ class AudioData(models.Model):
     """База данных для хранения данных о треках"""
     login = models.CharField(max_length=100, verbose_name='Имя пользователя')
     # slug = models.SlugField(max_length=100, db_index=True, verbose_name='URL')
-    trek_name = models.CharField(max_length=100, verbose_name='Имя трека')
+    audio_name = models.CharField(max_length=100, verbose_name='Имя трека')
     original_format = models.CharField(max_length=100, verbose_name='Формат оригинала')
-    original_track = models.CharField(max_length=100, verbose_name='Оригинальный трек')
-    convertable_track = models.CharField(max_length=100, verbose_name='Сконвертированный трек')
-    convertable_format = models.CharField(max_length=100, verbose_name='Формат сконвертированного трека')
+    original_path = models.CharField(max_length=100, verbose_name='Оригинальный трек')
+    convert_path = models.CharField(max_length=100, verbose_name='Сконвертированный трек')
+    convert_format = models.CharField(max_length=100, verbose_name='Формат сконвертированного трека')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата конвертации')
-    original_track_size_b = models.IntegerField(verbose_name="Оригинальный трек. B")
-    original_track_size_mb = models.FloatField(verbose_name="Оригинальный трек. Mb")
-    convert_track_size_b = models.IntegerField(verbose_name="Сконвертированный трек. b")
-    convert_track_size_mb = models.FloatField(verbose_name="Сконвертированный трек Mb")
+    original_audio_size_b = models.IntegerField(verbose_name="Оригинальный трек. B")
+    original_audio_size_mb = models.FloatField(verbose_name="Оригинальный трек. Mb")
+    convert_audio_size_b = models.IntegerField(verbose_name="Сконвертированный трек. b")
+    convert_audio_size_mb = models.FloatField(verbose_name="Сконвертированный трек Mb")
     deleted = models.BooleanField(default=False, verbose_name="Удален")
 
 
@@ -45,55 +46,69 @@ class AudioData(models.Model):
         verbose_name_plural = 'Треки пользователя'
         ordering = ['date',]
 
-    def get_convertable(self):
-        """Возвращет относительный путь к сконвертированному треку."""
+    def audiofile_name(self)->str:
+        """Возвращает имя аудиофайла."""
+        if self.audio_name:
+            return str(self.audio_name)
 
-        # Для отображения путей к трекам в личном кабинете с тегом static
-        if self.convertable_track:
-            audio = str(self.convertable_track) # << media//convertible_tracks/Пользватель/аудио.формат
-            path :str = audio[audio.rfind('convertible_tracks'):] # << convertible_tracks/Пользватель/аудио.формат
-
-            return path
-        else:
-            return None
-
-    def get_trek_name(self):
-        """Возвращает имя сконвертированного трека."""
-
-        if self.trek_name:
-            name :str = str(self.trek_name)
-            return name
-
-        else:
-            return None
-
-    def get_trek_format_convert(self):
-        """Возвращает формат трека."""
-        if self.convertable_format:
-            frmt = str(self.convertable_format).lower()
-            return frmt
-        else:
-            return None
-
-    def get_original(self):
-        """Возвращет относительный путь к оригинальному треку треку."""
-
-        # Для отображения путей к трекам в личном кабинете с тегом static
-        if self.original_track:
-            audio = str(self.original_track)  # << media/original_tracks/Пользватель/аудио.формат
-            path = audio[audio.rfind('original_track'):]  # << original_tracks/Пользватель/аудио.формат
-            return path
-
-        else:
-            return None
-
-
-    def get_trek_format_orig(self):
-        """Возвращает формат трека."""
-
+    def orig_audio_format(self)->str:
+        """Возвращает формат оригинального аудиофайла."""
         if self.original_format:
-            frmt = str(self.original_format).lower()
-            return frmt
-        else:
-            return None
+            return str(self.original_format)
+
+    def orig_audio_fullname(self)->str:
+        """Возвращает имя и формат оригинального аудиофайла."""
+        if self.original_path:
+            return f"{self.audio_name}.{self.original_format}"
+
+    def convert_audio_format(self)->str:
+        """Возвращает имя конвертированного аудиофайла."""
+        if self.convert_format:
+            return str(self.convert_format)
+
+    def convert_audio_fullname(self)->str:
+        """Возвращает имя и формат конвертированного аудиофайла."""
+        if self.convert_path:
+            return f"{self.audio_name}.{self.convert_format}"
+
+    def orig_audio_path(self)->str:
+        """Возвращает абсолютный путь к оригинальному аудиофайлу."""
+        if self.original_path:
+            return os.path.normpath(str(self.original_path))
+
+    def convert_audio_path(self)->str:
+        """Возвращает абсолютный путь к конвертированному аудиофайлу."""
+        if self.convert_path:
+            return os.path.normpath(str(self.convert_path))
+
+    def orig_aduio_relpath(self) -> str:
+        """Возвращает относительную ссылку на оригинальный аудиофайл."""
+        # Возвращает путь относительно media\original_tracks. <<< Guest\testsong1.mp3
+        if self.original_path:
+            audio_path :str = self.orig_audio_path()
+            audio_path :str = audio_path[audio_path.rfind('original_tracks'): ]
+            return audio_path
+
+    def convert_aduio_relpath(self) -> str:
+        """Возвращает относительную ссылку на конвертированный аудиофайл."""
+        # Возвращает путь относительно media\convertible_tracks. <<< Guest\testsong1.ac3
+        if self.original_path:
+            audio_path :str = self.convert_audio_path()
+            audio_path :str = audio_path[audio_path.rfind('convertible_tracks'): ]
+            return audio_path
+
+    def orig_size_mb(self)->float:
+        """Возвращает размер оригинального аудифоайла в мегабайтах."""
+        if self.original_audio_size_mb:
+            return float(str(self.original_audio_size_mb))
+
+    def convert_size_mb(self)->float:
+        """Возвращает размер конвертированного аудиофайла в мегабайтах."""
+        if self.convert_audio_size_mb:
+            return float(str(self.convert_audio_size_mb))
+
+    def convert_date(self):
+        """Возвращает дату и время конвертации."""
+        # return self.date
+        return str(self.date)
 
