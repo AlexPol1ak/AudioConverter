1472ee641ed73dcaf2364940c8bffe04896bdec7
Alex <zarjrw1994@gmail.com>
2022-10-16 23:42:45 +0300

    Added function to return audio for downloading.

--- AudioApp/utils/utils.py
+++ AudioApp/utils/utils.py
@@ -1,3 +1,8 @@
+import os
+
+from django.http import FileResponse
+
+
 def error_messages(form) ->dict:
     """ Возвращает список ошибок для пользователя, при загрузке не валидных данных"""
 
@@ -6,5 +11,14 @@ def error_messages(form) ->dict:
         errors[field] = form.errors[field].as_text().replace('* ', '')
 
     return errors
+
+def download_audio(audio_path :str) -> FileResponse:
+    """ Возвращает объект-аудиофайл в двоичном формате готовый для скачивания пользвателем. """
+
+    response = FileResponse(open(audio_path, 'rb'))
+    response['content_type'] = "application/octet-stream"
+    response['Content-Disposition'] = 'attachment; filename=' + os.path.basename(audio_path)
+
+    return response
 
 
