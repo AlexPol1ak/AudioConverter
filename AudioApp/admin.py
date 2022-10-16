06367296640954b12d9ab2c07cfd1cfbd864aeb5
Alex <zarjrw1994@gmail.com>
2022-10-16 23:44:48 +0300

    Displayed in the admin panel were added. Filters added.

--- AudioApp/admin.py
+++ AudioApp/admin.py
@@ -4,11 +4,11 @@ from .models import UserSong, AudioData
 class AudioDataAdmin(admin.ModelAdmin):
     """Класс настроек для отображения модели TrackData в админке"""
     # prepopulated_fields = {'slug': ('login',)}
-    list_display = ('login', 'trek_name', 'original_format','original_track_size_mb', 'convert_track_size_mb', 'deleted' ,'date', )
+    list_display = ('login', 'audio_name', 'original_format','original_audio_size_mb', 'convert_audio_size_mb', 'deleted' ,'date', )
     list_editable = ('deleted',)
     search_fields = ('login', 'date')
-    list_filter = ('login', 'deleted', 'original_format', 'convertable_format', 'date',)
-    readonly_fields = ('convert_track_size_b', 'original_track_size_b', 'original_track_size_mb', 'convert_track_size_mb')
+    list_filter = ('login', 'deleted', 'original_format', 'convert_format', 'date',)
+    readonly_fields = ('convert_audio_size_b', 'original_audio_size_b', 'original_audio_size_mb', 'convert_audio_size_mb')
 
 
 admin.site.register(UserSong)
